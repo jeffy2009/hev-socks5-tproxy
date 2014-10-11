2b8a79eaa9aaf10ee50e5fa3054e79de7e70046a
Heiher <r@hev.cc>
2014-10-11 13:41:27 +0800

    HevSocks5TProxy: Remove all sessions in finalize.

--- src/hev-socks5-tproxy.c
+++ src/hev-socks5-tproxy.c
@@ -37,6 +37,7 @@ struct _HevSocks5TProxy
 static bool listener_source_handler (HevEventSourceFD *fd, void *data);
 static bool timeout_source_handler (void *data);
 static void session_close_handler (HevSocks5Session *session, void *data);
+static void remove_all_sessions (HevSocks5TProxy *self);
 
 HevSocks5TProxy *
 hev_socks5_tproxy_new (HevEventLoop *loop, const char *laddr, unsigned short lport,
@@ -116,6 +117,7 @@ hev_socks5_tproxy_unref (HevSocks5TProxy *self)
 			hev_event_loop_del_source (self->loop, self->listener_source);
 			hev_event_loop_del_source (self->loop, self->timeout_source);
 			close (self->listen_fd);
+			remove_all_sessions (self);
 			HEV_MEMORY_ALLOCATOR_FREE (self);
 		}
 	}
@@ -191,5 +193,19 @@ session_close_handler (HevSocks5Session *session, void *data)
 				hev_socks5_session_get_source (session));
 	hev_socks5_session_unref (session);
 	self->session_list = hev_slist_remove (self->session_list, session);
+}
+
+static void
+remove_all_sessions (HevSocks5TProxy *self)
+{
+	HevSList *list = NULL;
+	for (list=self->session_list; list; list=hev_slist_next (list)) {
+		HevSocks5Session *session = hev_slist_data (list);
+		/* printf ("Remove session %p\n", session); */
+		hev_event_loop_del_source (self->loop,
+					hev_socks5_session_get_source (session));
+		hev_socks5_session_unref (session);
+	}
+	hev_slist_free (self->session_list);
 }
 
